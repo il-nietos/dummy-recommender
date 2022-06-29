038918696335557ed4313c9d3a7058d0ab9dd157
ilona-nietosvaara <i.nietosvaara@gmail.com>
2022-06-29 11:38:42 +0200

    take out trailing white space

--- recommender.py
+++ recommender.py
@@ -5,4 +5,3 @@ def recommend_random(movies, k=5):
     Dummy recommender that recommends a list of random movies. Ignores the actual query.
     """
     return movies['movieId'].sample(k).to_list()
-    
